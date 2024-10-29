212087ef135b7e41a7128067dd14239b99ccb25b
Niklas Pelli <niklaspelli22@gmail.com>
2024-10-29 10:24:19 +0100

    first commit

---
+++ src/routes/csrfRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { generateCsrfToken } = require("../utils/csrfUtils.js");
+const { verifyJwt } = require("../utils/jwtUtils.js");
+
+router.get("/token", verifyJwt, generateCsrfToken);
+
+module.exports = router;
+
+////
